ec0804126f67009aebd9e6d81a6e91a84738a8a8
Marcos Alberto Cosmoski Filho <marcosmoski@Marcoss-MBP.Home>
2022-06-30 08:59:29 -0300

    tsconfig e configuracoes

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        backgroundImage: { 
+          blur: 'url(/src/assets/blur_background.png)'
+        },
+        fontFamily: { 
+          sans: 'Roboto, sans-serif'
+        },
+        colors: {
+          danger: '#E25858',
+          green: {
+            300: '#00B37E',
+            500: '#00875F',
+            700: '#015F43',
+          },
+          blue: {
+            500: '#4EA8DE',
+            900: '#1E6F9F',
+          },
+          orange: {
+            500: '#FBA94C',
+          },
+          red: {
+            500: '#F75A68',
+          },
+          gray: {
+            100: '#F2F2F2',
+            200: '#D9D9D9',
+            300: '#808080',
+            400: '#333333',
+            500: '#262626',
+            600: '#1A1A1A',
+            700: '#0D0D0D',
+            900: '#09090A'
+          },
+          purple: {
+            500: '#8284FA',
+            900: '#5E60CE'
+          },
+        },
+      }
+    },
+  },
+  plugins: [],
+}
